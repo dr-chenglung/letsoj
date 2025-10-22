8263d07588a8c887ca2ecc25545949f053912b99
Cheng-Lung, Huang <clhuang@nkust.edu.tw>
2025-10-22 22:38:20 +0800

    Add files via upload

--- oj-web/website_configs/urls.py
+++ oj-web/website_configs/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("app_oj.urls")),
     path("account/", include("app_account.urls")), # for login, register
-    path('accounts/', include('django.contrib.auth.urls')), # for logout
+    path("accounts/logout/", include("app_account.urls")),  # 為了相容 {% url 'logout' %} 模板標籤
+    # path('accounts/', include('django.contrib.auth.urls')), # 已改用自訂的 logout，避免路由衝突
     path("manage/", include("app_management.urls")),
     path("admin/", admin.site.urls),
 ]
